f62b4c5b562437632bf66b7f04b3052c700b5d5d
vanz1220 <ije.malong@gmail.com>
2022-05-19 15:26:09 +0800

    header and banner edit

--- sections/banner.php
+++ sections/banner.php
@@ -6,6 +6,8 @@
           <h2 class="fw-light">DONT' WAIT!</h2>
           <h4 class="fw-light">LET START TO ORDER FOOD NOW</h4>
         </div>
+        <div class="col-12 banner-image-right">
+        </div>
       </div>
     </div>
 </div>
