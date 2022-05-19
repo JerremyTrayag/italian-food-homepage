3c6fd23650b936fccab41ffda52758686bcfe8c1
Glen Naboa <glen.naboa@ivystreet.com.au>
2022-05-19 08:56:58 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Italian Food</title>
+
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/banner.css">
+    <link rel="stylesheet" href="./css/menu.css">
+    <link rel="stylesheet" href="./css/events.css">
+    <link rel="stylesheet" href="./css/gallery.css">
+    <link rel="stylesheet" href="./css/reservation.css">
+    <link rel="stylesheet" href="./css/footer.css">
+</head>
+<body>
+    <?php
+        include('./sections/header.php');
+        include('./sections/banner.php');
+        include('./sections/menu.php');
+        include('./sections/events.php');
+        include('./sections/gallery.php');
+        include('./sections/reservation.php');
+        include('./sections/footer.php');
+    ?>
+</body>
+</html>
