12bf6c195e68038571f29454b855018508da715d
vanz1220 <ije.malong@gmail.com>
2022-05-19 14:07:14 +0800

    header and banner

--- sections/header.php
+++ sections/header.php
@@ -23,15 +23,3 @@
       </div>
     </div>
   </nav>
-
-  <header class="header-hero">
-    <div class="container h-100">
-      <div class="row h-100 align-items-center">
-        <div class="col-12 header-text-left">
-          <h1 class="fw-light">Are You Hungry?</h1>
-          <h2 class="fw-light">Are You Hungry?</h2>
-          <h4 class="fw-light">LET START TO ORDER FOOD NOW</h4>
-        </div>
-      </div>
-    </div>
-  </header>
