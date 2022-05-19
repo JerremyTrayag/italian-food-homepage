04d9795397d2b67dc325341d44f482214a011948
vanz1220 <ije.malong@gmail.com>
2022-05-19 14:18:02 +0800

    banner

--- sections/banner.php
+++ sections/banner.php
@@ -3,7 +3,7 @@
       <div class="row h-100 align-items-center">
         <div class="col-12 banner-text-left">
           <h1 class="fw-light">Are You Hungry?</h1>
-          <h2 class="fw-light">Are You Hungry?</h2>
+          <h2 class="fw-light">DONT' WAIT!</h2>
           <h4 class="fw-light">LET START TO ORDER FOOD NOW</h4>
         </div>
       </div>
