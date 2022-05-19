9e28ef941063f48d316dd33a4a192e4398dea49c
Glen Naboa <glen.naboa@ivystreet.com.au>
2022-05-19 13:56:09 +0800

    Move scripts to head tag

--- sections/header.php
+++ sections/header.php
@@ -1,10 +1,4 @@
 <!-- Navigation -->
-<!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-
 <nav class="navbar navbar-expand-lg navbar-light fixed-top">
     <div class="container">
       <a class="navbar-brand" href="#"><img src="/images/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top"></a>
