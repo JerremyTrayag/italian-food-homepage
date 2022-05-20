15962998889420f60c9100b5f9d492757860ab93
Jommel Dalisaymo <dalisaymo36@gmail.com>
2022-05-20 15:33:03 +0800

    Updated Events and Menu Sections

--- sections/events.php
+++ sections/events.php
@@ -1,1 +1,35 @@
-<h1>Updateeeeeeeeeeeeeeeeeeeeeeeeeee</h1>
+<div class="events-section py-5">
+    <header class="text-center container">
+        <h4 class="mb-0 text-uppercase">Discover</h4>
+        <h1 class="header-title">UPCOMING EVENTS</h1>
+        <p class="header-sub">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero modi dolorum officiis illum unde ipsum reiciendis dolorem magni facilis eaque est, nemo recusandae minus. Perspiciatis enim corporis nulla sapiente ut?</p>
+    </header>
+
+
+    <div class="events-carousel-container">
+        <div class="carousel first">
+            <div class="carousel-overlay"></div>
+            <img src="images/Image11.png" alt="">
+        </div>
+
+        <div class="carousel mid">
+            <img src="images/Image.jpg" alt="">
+        </div>
+
+        <div class="carousel last">
+            <div class="carousel-overlay"></div>
+            <img src="images/Image2.png" alt="">
+        </div>
+    </div>
+
+    <div class="container d-flex justify-content-center text-center">
+        <span class="dots d-flex pt-3">
+            <ul class="d-flex px-0 pt-3 pt-sm-3 pt-md-4 pt-lg-4">
+                <li class="list-unstyled mx-2"><a class="active" href="#"></a></li>
+                <li class="list-unstyled mx-2"><a href="#"></a></li>
+                <li class="list-unstyled mx-2"><a href="#"></a></li>
+            </ul>
+        </span>
+    </div>
+
+</div>
--- sections/menu.php
+++ sections/menu.php
@@ -1,1 +1,74 @@
-<h1>Menu</h1>
+<div class="menu-section py-5">
+    
+    <header class="text-center container">
+        <h1 class="header-title">CHOOSE & ENJOY</h1>
+        <p class="header-sub">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero modi dolorum officiis illum unde ipsum reiciendis dolorem magni facilis eaque est, nemo recusandae minus. Perspiciatis enim corporis nulla sapiente ut?</p>
+    </header>
+
+    <div class="row justify-content-center">
+
+        <div class="menu-background"></div>
+
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 py-3">
+
+            <div class="d-flex justify-content-center">
+                <img class="img-fluid menu-item-img" src="images/pasta2.png" alt="">
+            </div>
+
+            <div class="menu-item text-center pt-3">
+                <h4 class="menu-item-name text-uppercase fw-bolder">Lorem ipsum dolor</h4>
+                <p class="menu-item-description px-5">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
+                </p>
+
+                <div class="menu-item-btn">
+                    <a href="#" class="item-btn">order now</a>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 py-3">
+
+            <div class="d-flex justify-content-center">
+                <img class="img-fluid menu-item-img" src="images/bread1.png" alt="">
+            </div>
+
+            <div class="menu-item text-center pt-3">
+                <h4 class="menu-item-name text-uppercase fw-bolder">Lorem ipsum dolor</h4>
+                <p class="menu-item-description px-5">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
+                </p>
+
+                <div class="menu-item-btn">
+                    <a href="#" class="item-btn">order now</a>
+                </div>
+
+            </div>
+            
+        </div>
+
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 py-3">
+
+            <div class="d-flex justify-content-center">
+                <img class="img-fluid menu-item-img" src="images/pasta1.png" alt="">
+            </div>
+
+            <div class="menu-item text-center pt-3">
+                <h4 class="menu-item-name text-uppercase fw-bolder">Lorem ipsum dolor</h4>
+                <p class="menu-item-description px-5">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
+                </p>
+
+                <div class="menu-item-btn">
+                    <a href="#" class="item-btn">order now</a>
+                </div>
+
+            </div>
+            
+        </div>
+
+    </div>
+
+</div>
