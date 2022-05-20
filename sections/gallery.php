68925935e6564d07ebe3b9502c3ac0532b75d15b
Byron <byronedquibal.ifliba@gmail.com>
2022-05-20 15:58:50 +0800

    Updated Gallery and Reservation

--- sections/gallery.php
+++ sections/gallery.php
@@ -1,1 +1,72 @@
-<h1>Gallery</h1>
+
+
+<div class="container">
+
+
+            <div class="d-flex justify-content-center">
+
+                <div class="row align-items-center">
+
+                    <div class="col-6 col-sm-12 col-md-12 col-lg-6 mb-1">
+                            <div class="Gallery1">
+                                <img src ="images/Gallery1.png">
+                            </div>
+                    </div>
+
+                    <div class="col-6 col-sm-12 col-md-12 col-lg-6">
+                        
+                        <div class="Gallery2">
+                            <img src ="images/Gallery2.png" class="mb-2">
+                        </div>
+
+                        <div class="Gallery3">
+                            <img src ="images/Gallery3.png">
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- <div>
+
+            <div class="row justify-content-center align-items-center">
+
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+
+                    <div class="row">
+
+                        <div class="col-12">
+
+                        <div class="Gallery1">
+                            <img src ="images/Gallery1.png">
+                        </div>
+
+                    </div>
+                        
+                </div>
+                    
+                </div>
+
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+
+                    <div class="row">
+
+                            <div class="col-12">
+
+                                <div class="Gallery2">
+                                    <img src ="images/Gallery2.png" class="mb-2">
+                                </div>
+
+                                <div class="Gallery3">
+                                    <img src ="images/Gallery3.png">
+                                </div>
+                            </div>
+                            
+                    </div>
+            </div>
+           
+    </div> -->
+
+
+</div>
--- sections/reservation.php
+++ sections/reservation.php
@@ -1,1 +1,26 @@
-<h1>Reservation</h1>
+
+<div class="container mt-5">
+    <div class="text-center mb-5">
+        <h4 >RESERVATION</h4>
+        <h1>BOOK YOUR TABLE</h1>
+    </div>
+
+    <div>
+
+            <div class="row justify-content-center">
+
+                <div class="col-8 col-md-6 col-lg-4">
+                    <input type="text" class="form-control  mb-2" id="exampleFormControlInput1" placeholder="Name">
+                    <input type="text" class="form-control  mb-2" id="exampleFormControlInput1" placeholder="Date">
+                    <input type="text" class="form-control  mb-2" id="exampleFormControlInput1" placeholder="People">
+                </div>
+
+                <div class="col-8 col-md-6 col-lg-4">
+                    <input type="text" class="form-control  mb-2" id="exampleFormControlInput1" placeholder="Email">
+                    <input type="text" class="form-control  mb-2" id="exampleFormControlInput1" placeholder="Time">
+                    <button type="button" class="btn btn-warning w-100"><b>FIND A TABLE</b></button>
+                </div>
+            </div>
+           
+    </div>
+</div>
