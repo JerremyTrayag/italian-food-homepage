5358adced3b74ce06b9b7040deb4185e5aab3f48
vanz1220 <ije.malong@gmail.com>
2022-05-23 10:47:46 +0800

    added images

--- sections/banner.php
+++ sections/banner.php
@@ -1,6 +1,9 @@
 <div class="banner-hero">
     <div class="container h-100">
       <div class="row h-100">
+        <div class="col-6 banner-image-left-leaf1">
+          <img class="img-fluid" src="../italian-food-homepage/images/leaf1.png" alt="leaf1">
+        </div>
         <div class="col-6 banner-text-left d-flex align-items-center">
           <div>
             <h1 class="header-heading-1 fw-light">Are You Hungry?</h1>
