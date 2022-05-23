b8fdabe24a6dc3e10d9713ecc0117898fc804582
Glen Naboa <glen.naboa@ivystreet.com.au>
2022-05-23 09:36:16 +0800

    Fluid Image

--- sections/banner.php
+++ sections/banner.php
@@ -18,7 +18,7 @@
           </div>
         </div>
         <div class="col-6 banner-image-right d-flex align-items-center">
-          <img src="../images/plate.png" alt="food4you">
+          <img class="img-fluid" src="../images/plate.png" alt="food4you">
         </div>
       </div>
     </div>
