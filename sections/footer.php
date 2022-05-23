4ce992ec9f949d0164dd213d4a3345524517efe5
Jommel Dalisaymo <dalisaymo36@gmail.com>
2022-05-23 10:40:08 +0800

    Updated the footer section

--- sections/footer.php
+++ sections/footer.php
@@ -1,28 +1,31 @@
 <div class="footer-section py-5 bg-dark">
-    <div class="container">
+    <div class="footer-overlay"></div>
+    <div class="container footer-content">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 d-flex justify-content-center justify-content-sm-center justify-content-md-start justify-content-lg-start">
                 <img src="images/logo.png" alt="">
             </div>
-            <div class="row align-middle py-5">
-                <div class="col-6">
+
+            <div class="row py-5">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 text-center text-sm-center text-md-start text-lg-start">
                     <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptatem aspernatur delectus corporis in earum, a aliquam optio perferendis repudiandae, unde architecto temporibus nemo aperiam sit voluptas reiciendis! Id, placeat.</p>
                 </div>
-                <div class="col-6">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 pt-sm-2">
                     <div class="footer-contact d-flex justify-content-center align-items-center">
                         <ul class="list-unstyled">
-                            <li class="d-flex justify-content-around"><i class="fas fa-map-marker-alt footer-logo"></i><span class="text-white"> Main Road, Building Name, Country</span></li>
-                            <li class=""><i class="fas fa-envelope footer-logo"></i><span class="text-white"> info@companyname.com</span></li>
+                            <li class="my-2 text-center text-sm-center text-md-start text-lg-start"><i class="fas fa-map-marker-alt footer-logo"></i> <span class="text-white">Main Road, Building Name, Country</span></li>
+                            <li class="my-2 text-center text-sm-center text-md-start text-lg-start"><i class="fas fa-envelope footer-logo"></i> <span class="text-white">info@companyname.com</span></li>
                         </ul>
                     </div>
                 </div>
             </div>
+
             <div class="row pt-5">
-                <div class="col-6">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 text-center text-sm-center text-md-start text-lg-start">
                     <p class="text-white">© Company Name 2020. All rights reserved.</p>
                 </div>
-                <div class="col-6 footer-sm-container">
-                    <ul class="d-flex list-unstyled justify-content-end">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 footer-sm-container">
+                    <ul class="d-flex list-unstyled justify-content-center justify-content-sm-center justify-content-md-end justify-content-lg-end">
                         <li class="mx-2"><a href="#"><i class="fa-brands fa-instagram footer-social-icon"></i></a></li>
                         <li class="mx-2"><a href="#"><i class="fa-brands fa-facebook-f footer-social-icon"></i></a></li>
                         <li class="mx-2"><a href="#"><i class="fa-brands fa-twitter footer-social-icon"></i></a></li>
