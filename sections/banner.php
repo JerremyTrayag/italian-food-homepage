0180e29deb37520699403ee93a1b4c3babed2a87
vanz1220 <ije.malong@gmail.com>
2022-05-23 11:36:51 +0800

    changes in margin of images

--- sections/banner.php
+++ sections/banner.php
@@ -4,6 +4,9 @@
         <div class="col-6 banner-image-left-leaf1">
           <img class="img-fluid" src="../italian-food-homepage/images/leaf1.png" alt="leaf1">
         </div>
+        <div class="col-6 banner-image-right-pasta">
+          <img class="img-fluid" src="../italian-food-homepage/images/pasta.png" alt="pasta">
+        </div>
         <div class="col-6 banner-text-left d-flex align-items-center">
           <div>
             <h1 class="header-heading-1 fw-light">Are You Hungry?</h1>
@@ -22,6 +25,15 @@
         </div>
         <div class="col-6 banner-image-right d-flex align-items-center">
           <img class="img-fluid" src="../italian-food-homepage/images/plate.png" alt="food4you">
+        </div>
+        <div class="col-6 banner-image-bottom-left-spag">
+          <img class="img-fluid" src="../italian-food-homepage/images/spag.png" alt="spag">
+        </div>
+        <div class="col-6 banner-image-bottom-center-tomato">
+          <img class="img-fluid" src="../italian-food-homepage/images/tomato.png" alt="tomato">
+        </div>
+        <div class="col-6 banner-image-bottom-right-leaf2">
+          <img class="img-fluid" src="../italian-food-homepage/images/leaf2.png" alt="leaf2">
         </div>
       </div>
     </div>
